966298d35b1ee609354412f0ce993390524b8a69
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-12-28 13:50:01 -0600

    added travis.yml, config/database.yml.travis

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :users
+	resources :pills
+
+	namespace :api do
+	  resources :pills
+	end
+	
+	#signin and sign out paths
+	get 'sign_in' => 'sessions#new', as: :sign_in
+	post 'sign_in' => 'sessions#create'
+	delete 'sign_in' => 'sessions#delete', as: :end_session
+	get 'sign_out' => 'sessions#delete', as: :sign_out
+
+	root 'home#hello'
+
+
 end
