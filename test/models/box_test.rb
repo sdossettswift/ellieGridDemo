5eacdc4a7f75688fc1cb6a49de97c815f474ab90
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-12-28 23:11:56 -0600

    Removed feature and fixture tests

--- test/models/box_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe Box do
-  let(:box) { Box.new }
-
-  it "must be valid" do
-    value(box).must_be :valid?
-  end
-end
--- test/models/pill_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe Pill do
-  let(:pill) { Pill.new }
-
-  it "must be valid" do
-    value(pill).must_be :valid?
-  end
-end
